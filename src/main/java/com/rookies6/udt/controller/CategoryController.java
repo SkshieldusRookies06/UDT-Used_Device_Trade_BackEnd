cc88135aafc71e02865e9dfaed6b2b2a4537704c
JonghyeonWon <wonjonghyeon5646@naver.com>
2026-09-22 12:18:03 +0900

    feat(product): 카테고리 조í실구현 [ãT-006]

--- src/main/java/com/rookies6/udt/controller/CategoryController.java
+++ src/main/java/com/rookies6/udt/controller/CategoryController.java
@@ -5,22 +5,30 @@ package com.rookies6.udt.controller;
 import com.rookies6.udt.common.ApiResponse;
 import com.rookies6.udt.dto.CategoryResponse;
 import java.util.List;
+
+import com.rookies6.udt.service.CategoryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/categories")
+@RequiredArgsConstructor
 public class CategoryController {
+
+    private final CategoryService categoryService;
 
     @GetMapping
     public ApiResponse<List<CategoryResponse>> list() {
-        List<CategoryResponse> categories = List.of(
-                new CategoryResponse("1", "노트북"),
-                new CategoryResponse("2", "스마트폰"),
-                new CategoryResponse("3", "태블릿"),
-                new CategoryResponse("4", "이어폰"),
-                new CategoryResponse("5", "기타"));
+//        List<CategoryResponse> categories = List.of(
+//                new CategoryResponse("1", "노트북"),
+//                new CategoryResponse("2", "스마트폰"),
+//                new CategoryResponse("3", "태블릿"),
+//                new CategoryResponse("4", "이어폰"),
+//                new CategoryResponse("5", "기타"));
+
+        List<CategoryResponse> categories = categoryService.getCategories();
         return ApiResponse.of(categories, "카테고리 조회가 완료되었습니다");
     }
 }
