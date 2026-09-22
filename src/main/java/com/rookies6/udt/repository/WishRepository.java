812aed89d967736ac0a3259e9ce41fbdc6f80b99
JonghyeonWon <wonjonghyeon5646@naver.com>
2026-09-22 14:05:14 +0900

    feat(product): 상품 목록/상세 API 실구현 [T-006]

--- src/main/java/com/rookies6/udt/repository/WishRepository.java
+++ src/main/java/com/rookies6/udt/repository/WishRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface WishRepository extends JpaRepository<Wish, Long> {
 
     boolean existsByUserIdAndProductId(Long userId, Long productId);
+    int countByProductId(Long productId);
+
 }
